4b9674f4d2aaf3936c01a8f9064a5130fad93735
Johannes Zeller <johannes_zeller@sluz.ch>
2023-09-23 10:15:25 +0200

    solve task 3

---
+++ Average.Test/AverageTest.cs
@@ -0,0 +1,57 @@
+﻿namespace Average.Test
+{
+    public class AverageTest
+    {
+        [Fact]
+        public void TestComputeMeanOfFile()
+        {
+            string tempFilePath = Path.Combine(Path.GetTempPath(), "numbers.txt");
+            StreamWriter writer = new(tempFilePath);
+            writer.WriteLine("1");
+            writer.WriteLine("2");
+            writer.WriteLine("3");
+            writer.Close();
+
+            double actual = Average.ComputeMeanOfFile(tempFilePath);
+
+            Assert.Equal(2, actual);
+
+            File.Delete(tempFilePath);
+        }
+
+        [Fact]
+        public void TestComputeMedianOfFile()
+        {
+            string tempFilePath = Path.Combine(Path.GetTempPath(), "numbers.txt");
+            StreamWriter writer = new(tempFilePath);
+            writer.WriteLine("23");
+            writer.WriteLine("29");
+            writer.WriteLine("25");
+            writer.WriteLine("-142");
+            writer.Close();
+
+            double actual = Average.ComputeMedianOfFile(tempFilePath);
+
+            Assert.Equal(24, actual);
+
+            File.Delete(tempFilePath);
+        }
+
+        [Fact]
+        public void TestComputeModeOfFile()
+        {
+            string tempFilePath = Path.Combine(Path.GetTempPath(), "numbers.txt");
+            StreamWriter writer = new(tempFilePath);
+            writer.WriteLine("23");
+            writer.WriteLine("29");
+            writer.WriteLine("23");
+            writer.Close();
+
+            List<int> actual = Average.ComputeModeOfFile(tempFilePath);
+
+            Assert.Equal(new[] { 23 }, actual);
+
+            File.Delete(tempFilePath);
+        }
+    }
+}
